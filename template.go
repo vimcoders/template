5ee061e49b1dd8779f2fe850cb2d7d1943309c42
vimcoders <vimcoders@gmail.com>
2021-09-23 12:08:02 +0000

    template

--- template.go
+++ template.go
@@ -1,6 +1,7 @@
 package gen
 
 import (
+	"encoding/csv"
 	"text/template"
 )
 
@@ -9,14 +10,33 @@ type Template struct {
 	*template.Template
 }
 
-func NewTemplate(csvPath string) (*Template, error) {
+func NewTemplate(r *csv.Reader) (*Template, error) {
 	tmpl, err := template.ParseFiles("./cfg.template")
 
 	if err != nil {
 		return nil, err
 	}
 
+	type Input struct {
+		Name   string
+		Header map[string]string
+		Rows   [][]string
+	}
+
+	records, err := r.ReadAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	input := Input{"test", make(map[string]string), records[2:]}
+
+	for i, record := range records[0] {
+		input.Header[record] = records[1][i]
+	}
+
 	return &Template{
 		Template: tmpl,
+		input:    &input,
 	}, nil
 }
