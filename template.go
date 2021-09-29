d7042eb17e730287a59a03c5a2b3d76d5d6967c7
vimcoders <vimcoders@gmail.com>
2021-09-29 06:32:57 +0000

    template

--- template.go
+++ template.go
@@ -6,17 +6,18 @@ import (
 	"text/template"
 )
 
+type Input struct {
+	Row    string
+	Rows   string
+	Header []string
+	Data   [][]string
+}
+
 func NewTemplate(src *csv.Reader) (*template.Template, interface{}, error) {
 	tmpl, err := template.ParseFiles("./cfg.template")
 
 	if err != nil {
 		return nil, nil, err
-	}
-
-	type Input struct {
-		Name   string
-		Header map[string]string
-		Rows   [][]string
 	}
 
 	records, err := src.ReadAll()
@@ -25,12 +26,10 @@ func NewTemplate(src *csv.Reader) (*template.Template, interface{}, error) {
 		return nil, nil, err
 	}
 
-	fmt.Println(records)
-
-	input := Input{"NetRow", make(map[string]string), records[2:]}
+	input := Input{Row: "NetRow", Rows: "NetRows", Data: records[2:]}
 
 	for i, record := range records[0] {
-		input.Header[record] = records[1][i]
+		input.Header = append(input.Header, fmt.Sprintf("%v %v", record, records[1][i]))
 	}
 
 	return tmpl, &input, nil
